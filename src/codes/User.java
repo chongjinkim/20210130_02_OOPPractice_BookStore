80c90c7521ea90b6e4bad5b99041953c197843a6
김종진 <martia0118@gmail.com>
2021-01-30 12:17:37 +0900

    사용자 정보 출력 기능 추가

--- src/codes/User.java
+++ src/codes/User.java
@@ -1,16 +1,33 @@
 package codes;
 
 public class User {
-	
-	//멤버변수에 값 대입을 안할 경우에는, 기본값 대임 
-	//ex) String - null, int - 0, boolean - false;
-	
+
+	// 멤버변수에 값 대입을 안할 경우에는, 기본값 대임
+	// ex) String - null, int - 0, boolean - false;
+
 	String name = "미입력";
-	int birthYear = 0; //생년이 0이면 미입력으로 간주를 한다.
-	int point = 0;//기본값은 충전 포인트 없는 상태. 0원
-	
-	
-	
-	
-	
+	int birthYear = 0; // 생년이 0이면 미입력으로 간주를 한다.
+	int point = 0;// 기본값은 충전 포인트 없는 상태. 0원
+
+	// 기능 1. 사용자 기본 정보 출력
+	// 결과자료형 : 없음,
+	// 이름 : printUserInfo
+	// 실행 시 필요한 재료? 없다.
+
+	void printUserInfo() {
+		// 출력형태
+		// ========사용자정보========//
+		// 이름 : 김종진
+		// 나이 : 31세(2021년 기준)
+		// 보유 포인트 : 10000P
+		// ========================
+
+		System.out.println("=======사용자정보======");
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + "??세");
+		System.out.println("보유 포인트 : " + point + "P");
+		System.out.println("====================");
+
+	}
+
 }
