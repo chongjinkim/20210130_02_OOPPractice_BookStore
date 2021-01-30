d6f68c52f7c058efbd6071eb77bfc90123ded395
김종진 <martia0118@gmail.com>
2021-01-30 12:29:10 +0900

    사용자 정보 출력 시에 나이 계산해서 출력

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -13,20 +13,19 @@ public class MainDrive {
 		u1.name = "김민수";
 		u1.birthYear = 1995;
 		u1.point = 10000;
-		
-		//사용자 정보 출력
+
+		// 사용자 정보 출력
 		u1.printUserInfo();
-		
-		//두 번째 사용자, 이름 : 이수민, 출생년도 : 2010, 보유 포인트 : 2000P
-		
+
+		// 두 번째 사용자, 이름 : 이수민, 출생년도 : 2010, 보유 포인트 : 2000P
+
 		User u2 = new User();
-		
-		u2.name= "이수민";
+
+		u2.name = "이수민";
 		u2.birthYear = 2010;
 		u2.point = 2000;
-		
+
 		u2.printUserInfo();
-				
 
 	}
 
--- src/codes/User.java
+++ src/codes/User.java
@@ -24,25 +24,25 @@ public class User {
 
 		System.out.println("=======사용자정보======");
 		System.out.println("이름 : " + name);
-		System.out.println("나이 : " + "??세");
+		//나이는 따로 계산하지 않고, 클래스에 만들어둔 메쏘드 활용
+		System.out.println("나이 : " + getKoreanAge(2021) +"세");
 		System.out.println("보유 포인트 : " + point + "P");
 		System.out.println("====================");
 
 	}
-	
+
 	// 기능 2. 사용자의 나이 계산 기능.
-	// 결과자료형 : int 
+	// 결과자료형 : int
 	// 이름 : getKoreanAge
 	// 실행 시 필요한 재료? 몇년도에 대한 나이가 궁금한지, 년도를 받아서 계산,
-	
+
 	int getKoreanAge(int year) {
-		
-		int myAge = year - birthYear + 1;	
-		
-	// 계산 결과를 이 메쏘드의 결과로 지정
-		
+
+		int myAge = year - birthYear + 1;
+
+		// 계산 결과를 이 메쏘드의 결과로 지정
+
 		return myAge;
 	}
-	
 
 }
