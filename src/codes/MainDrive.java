f5fdf258dce56b0b251d58ff49b1951d423cc90a
김종진 <martia0118@gmail.com>
2021-01-30 12:21:58 +0900

    Update MainDrive.java

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,9 +1,33 @@
 package codes;
 
 public class MainDrive {
-	
+
 	public static void main(String[] args) {
+
+		User u1 = new User();
+
+		// 첫 사용자. 이름 - 김민수
+		// 출생년도 : 1995
+		// 보유포인트 : 10000
+
+		u1.name = "김민수";
+		u1.birthYear = 1995;
+		u1.point = 10000;
 		
+		//사용자 정보 출력
+		u1.printUserInfo();
+		
+		//두 번째 사용자, 이름 : 이수민, 출생년도 : 2010, 보유 포인트 : 2000P
+		
+		User u2 = new User();
+		
+		u2.name= "이수민";
+		u2.birthYear = 2010;
+		u2.point = 2000;
+		
+		u2.printUserInfo();
+				
+
 	}
 
 }
